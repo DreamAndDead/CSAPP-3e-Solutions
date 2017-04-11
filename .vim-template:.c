c8600eb1eebad71f2f5c0f8fe8999d799838bd90
zhangdawei <dawei.zhang@zuolin.com>
2017-04-11 13:38:14 +0800

    update

---
+++ .vim-template:.c
@@ -0,0 +1,13 @@
+/*
+ * %FILE%
+ */
+#include <stdio.h>
+#include <assert.h>
+
+%HERE%
+
+int main(int argc, char* argv[]) {
+  return 0;
+}
+
+
---
+++ chapter2/code/int-size-is-32.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <assert.h>
+
+int bad_int_size_is_32() {
+  int set_msb = 1 << 31;
+  int beyond_msb = 1 << 32;
+
+  return set_msb && !beyond_msb;
+}
+
+int int_size_is_32() {
+  int set_msb = 1 << 31;
+  int beyond_msb = 1 << 31 << 1;
+
+  return set_msb && !beyond_msb;
+}
+
+int int_size_is_32_for_16bit() {
+  int set_msb = 1 << 15 << 15 << 1;
+  int beyond_msb = set_msb << 1;
+
+  return set_msb && !beyond_msb;
+}
+
+int main(int argc, char *argv[]) {
+  assert(bad_int_size_is_32());
+  assert(int_size_is_32());
+  assert(int_size_is_32_for_16bit());
+  return 0;
+}

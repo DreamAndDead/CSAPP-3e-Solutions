89c982cfaae5b371aae2811c5366271401f6a281
DreamAndDead <favorofife@yeah.net>
2017-04-13 22:38:59 +0800

    fix code no warnings

--- chapter2/code/int-size-is-32.c
+++ chapter2/code/int-size-is-32.c
@@ -2,12 +2,14 @@
 #include <assert.h>
 
 /* The following function does not run properly on some machine */
+/*
 int bad_int_size_is_32() {
   int set_msb = 1 << 31;
   int beyond_msb = 1 << 32;
 
   return set_msb && !beyond_msb;
 }
+*/
 
 int int_size_is_32() {
   int set_msb = 1 << 31;
@@ -24,7 +26,6 @@ int int_size_is_32_for_16bit() {
 }
 
 int main(int argc, char *argv[]) {
-  assert(bad_int_size_is_32());
   assert(int_size_is_32());
   assert(int_size_is_32_for_16bit());
   return 0;

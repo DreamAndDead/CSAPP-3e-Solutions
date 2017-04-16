96c3a947f460a66d46a18029014b767377f4ab1c
DreamAndDead <favorofife@yeah.net>
2017-04-16 09:31:06 +0800

    add 2.82.c

--- chapter2/code/2.82.c
+++ chapter2/code/2.82.c
@@ -4,22 +4,56 @@
 #include <stdio.h>
 #include <assert.h>
 #include <limits.h>
-#include <time.h>
-#include <stdlib.h>
 #include "lib/random.h"
 
+/* broken when x is INT_MIN */
 int A(int x, int y) {
   return (x < y) == (-x > -y);
 }
 
+/*
+ * correct
+ *
+ * ((x + y) << 4) + y - x
+ *   =>
+ * x << 4 - x + y << 4 + y
+ *   =>
+ * x*16 - x + y*16 + y
+ *   whether overflow or not, =>
+ * x*15 + y*17
+ */
 int B(int x, int y) {
   return ((x + y) << 4) + y - x == 17 * y + 15 * x;
 }
 
+/*
+ * correct
+ *
+ * ~x + ~y + 1
+ *   =>
+ * ~x + 1 + ~y + 1 - 1
+ *   =>
+ * -x + -y - 1
+ *   =>
+ * -(x + y) - 1
+ *   =>
+ * ~(x + y) + 1 - 1
+ *   =>
+ * ~(x + y)
+ */
 int C(int x, int y) {
   return ~x + ~y + 1 == ~(x + y);
 }
 
+/*
+ * correct
+ *
+ * (ux - uy) == -(unsigned) (y - x)
+ *   =>
+ * -(ux - uy) == (unsigned) (y - x)
+ *   =>
+ * (ux - uy) == (unsigned) (x - y)
+ */
 int D(int x, int y) {
   unsigned ux = (unsigned) x;
   unsigned uy = (unsigned) y;
@@ -27,16 +61,31 @@ int D(int x, int y) {
   return (ux - uy) == -(unsigned) (y - x);
 }
 
+/*
+ * correct
+ *
+ * x >> 2 << 2
+ *   =>
+ * x & ~0x3
+ *   =>
+ * x - num(00/01/10/11)
+ *   =>
+ * ((x >> 2) << 2) <= x
+ */
 int E(int x, int y) {
   return ((x >> 2) << 2) <= x;
 }
 
 int main(int argc, char* argv[]) {
-  srand(time(NULL));
-
+  init_seed();
   int x = random_int();
   int y = random_int();
+
+  assert(!A(INT_MIN, 0));
   assert(B(x, y));
+  assert(C(x, y));
+  assert(D(x, y));
+  assert(E(x, y));
   return 0;
 }
 

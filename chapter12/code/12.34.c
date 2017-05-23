5f1934e43bde2eaabe47f4ecec0fb06e082ec61a
DreamAndDead <aquairain@gmail.com>
2017-05-23 15:23:37 +0800

    update chapter 12

---
+++ chapter12/code/12.34.c
@@ -0,0 +1,59 @@
+/*
+ * 12.34.c
+ */
+#include <stdio.h>
+#include "csapp.h"
+#include "12.34.h"
+
+int M1[N][M];
+int M2[N][M];
+
+int MUL12[N][M] = { 0 };
+
+void non_concurrent_mul(void) {
+  int i, j, k;
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++) {
+      int sum = 0;
+      for (k = 0; k < M; k++) {
+        sum += M1[i][k] * M2[k][j];
+      }
+      MUL12[i][j] = sum;
+    }
+}
+
+void *thread_mul(void *vargp) {
+  int idx = *(int*)vargp;
+  int start = ROWS_PER_THREAD * idx;
+  int i, j, k;
+  for (i = start; i < start+ROWS_PER_THREAD; i++)
+    for (j = 0; j < N; j++) {
+      int sum = 0;
+      for (k = 0; k < M; k++) {
+        sum += M1[i][k] * M2[k][j];
+      }
+      MUL12[i][j] = sum;
+    }
+}
+
+void concurrent_mul(void) {
+  pthread_t tid[THREAD];
+  int param[THREAD];
+  int i;
+
+  for (i = 0; i < THREAD; i++) {
+    param[i] = i;
+    Pthread_create(&tid[i], NULL, thread_mul, &param[i]);
+  }
+  for (i = 0; i < THREAD; i++) {
+    Pthread_join(tid[i], NULL);
+  }
+}
+
+int main(int argc, char* argv[]) {
+  /*non_concurrent_mul();*/
+  concurrent_mul();
+  return 0;
+}
+
+
---
+++ chapter12/code/12.34.h
@@ -0,0 +1,10 @@
+/*
+ * 12.34.h
+ */
+
+#define N 640
+#define M 640
+
+#define THREAD (1<<4)
+#define ROWS_PER_THREAD (N / THREAD)
+

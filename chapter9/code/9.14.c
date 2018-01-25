04dd49c05b3e0ae1e494db177c00441d8b4027f6
DreamAndDead <favorofife@yeah.net>
2018-01-25 20:02:16 +0800

    improve 9.14 test code

--- chapter9/code/9.14.c
+++ chapter9/code/9.14.c
@@ -2,9 +2,29 @@
  * 9.14.c
  */
 #include <stdio.h>
+#include <assert.h>
 #include "vm/csapp.h"
 
+int test(char* filename, char* content) {
+  int fd;
+  char buf[20];
+  fd = Open(filename, O_RDONLY, 0);
+  Read(fd, buf, strlen(content));
+  return !strncmp(buf, content, strlen(content));
+}
+
+int touch(char* filename, char* content) {
+  int fd;
+  umask(DEF_UMASK);
+  fd = Open(filename, O_WRONLY|O_CREAT|O_TRUNC, DEF_MODE);
+  Write(fd, content, strlen(content));
+  Close(fd);
+}
+
 int main(int argc, char* argv[]) {
+  touch("hello.txt", "Hello, world!");
+  assert(test("hello.txt", "Hello, world!"));
+
   struct stat stat;
   int fd;
   char* bufp;
@@ -18,6 +38,7 @@ int main(int argc, char* argv[]) {
   *bufp = 'J';
   Munmap(bufp, size);
 
+  assert(test("hello.txt", "Jello, world!"));
   return 0;
 }
 

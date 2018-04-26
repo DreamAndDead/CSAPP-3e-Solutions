9d75393b07f17ea2a36b9753af5345d4eace4110
DreamAndDead <favorofife@yeah.net>
2018-04-26 15:57:46 +0800

    fix 2.84

--- chapter2/code/float-le.c
+++ chapter2/code/float-le.c
@@ -15,10 +15,15 @@ int float_le(float x, float y) {
   unsigned sx = ux >> 31;
   unsigned sy = uy >> 31;
 
-  return sx == sy ? (sx == 0 ? ux <= uy : ux >= uy) : sx > sy;
+  // ref: https://github.com/DreamAndDead/CSAPP-3e-Solutions/issues/1
+  return (ux << 1 == 0 && uy << 1 == 0) || /* both zeros */
+    (sx && !sy) ||                         /* x < 0, y >= 0 or x <= 0, y > 0 */
+    (!sx && !sy && ux <= uy) ||            /* x > 0, y >= 0 or x >= 0, y > 0 */
+    (sx && sy && ux >= uy);                /* x < 0, y <= 0 or x <= 0, y < 0 */
 }
 
 int main(int argc, char* argv[]) {
+  assert(float_le(-0, +0));
   assert(float_le(+0, -0));
   assert(float_le(0, 3));
   assert(float_le(-4, -0));

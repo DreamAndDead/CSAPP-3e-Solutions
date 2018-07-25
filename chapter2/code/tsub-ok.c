664a9a5b29d2b068432b136cc72bd1f1dde353ab
aQua <aQua@aQuaYi.com>
2018-07-25 17:05:10 +0800

    Update tsub-ok.c
    消除了代码中的 ">" 和 "<" 号

--- chapter2/code/tsub-ok.c
+++ chapter2/code/tsub-ok.c
@@ -2,27 +2,60 @@
  * tsub-ok.c
  */
 #include <stdio.h>
-#include <assert.h>
 #include <limits.h>
+#include <assert.h>
 
-/* Determine whether arguments can be substracted without overflow */
-int tsub_ok(int x, int y) {
-  if (y == INT_MIN) {
+int tsub_ok(int x, int y)
+{
+    int is_ok = 1;
+
+    is_ok &= (y != INT_MIN);
+
+    int sub = x - y;
+    int sig_mask = INT_MIN;
+
+    int x_is_neg = x & sig_mask;
+    int x_is_pos = !x_is_neg && x != 0;
+    int y_is_neg = y & sig_mask;
+    int y_is_pos = !y_is_neg && y != 0;
+    int sub_is_not_neg = !(sub & sig_mask);
+    int sub_is_not_pos = (sub & sig_mask) | sub == 0;
+
+    int pos_over = x_is_pos && y_is_neg && sub_is_not_pos;
+    int neg_over = x_is_neg && y_is_pos && sub_is_not_neg;
+
+    is_ok &= !pos_over;
+    is_ok &= !neg_over;
+
+    return is_ok;
+}
+
+int main()
+{
+
+    // y 为 INT_MIN 导致溢出
+    int x = 0x12345678;
+    int y = INT_MIN;
+    printf("tsub_ok(0x%08x, 0x%08x) = %d\n", x, y, tsub_ok(x, y));
+    assert(!tsub_ok(x, y));
+
+    // sub 为正溢出
+    x = INT_MAX;
+    y = INT_MIN + 1;
+    printf("tsub_ok(0x%08x, 0x%08x) = %d\n", x, y, tsub_ok(x, y));
+    assert(!tsub_ok(x, y));
+
+    // sub 为负溢出
+    x = INT_MIN;
+    y = INT_MAX;
+    printf("tsub_ok(0x%08x, 0x%08x) = %d\n", x, y, tsub_ok(x, y));
+    assert(!tsub_ok(x, y));
+
+    // sub is ok
+    x = 0x77654321;
+    y = 0x12345678;
+    printf("tsub_ok(0x%08x, 0x%08x) = %d\n", x, y, tsub_ok(x, y));
+    assert(tsub_ok(x, y));
+
     return 0;
-  }
-
-  int neg_y = -y;
-  int sum = x + neg_y;
-  int pos_over = x > 0 && neg_y > 0 && sum < 0;
-  int neg_over = x < 0 && neg_y < 0 && sum >= 0;
-
-  return !(pos_over || neg_over);
 }
-
-int main(int argc, char* argv[]) {
-  assert(!tsub_ok(0x00, INT_MIN));
-  assert(tsub_ok(0x00, 0x00));
-  return 0;
-}
-
-

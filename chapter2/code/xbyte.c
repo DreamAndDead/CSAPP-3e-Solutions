47340472e4b6ff48b4b91d95967a7990d7b15d11
DreamAndDead <DreamAndDead@users.noreply.github.com>
2021-05-25 04:30:21 +0000

    deploy: 608b9662a9ddaedd62c69dc708972342a1f44829

--- chapter2/code/xbyte.c
+++ chapter2/code/xbyte.c
@@ -1,29 +1,24 @@
-/*
- * xbyte.c
- */
 #include <stdio.h>
 #include <assert.h>
 
 typedef unsigned packet_t;
 
 int xbyte(packet_t word, int bytenum) {
-  /*
-   * pay attention when byte we want is negetive
-   *
-   * Assume sizeof(unsigned) is 4
-   * first shift left 8 * (4 - 1 - bytenum)
-   * then arthemetic shift right 8 * (4 - 1) reserve signficant bit
-   */
-  int size = sizeof(unsigned);
-  int shift_left_val = (size - 1 - bytenum) << 3;
-  int shift_right_val = (size - 1) << 3;
-  return (int) word << shift_left_val >> shift_right_val;
+  int max_bytenum = 3;
+  return (int) word << ((max_bytenum - bytenum) << 3) >> (max_bytenum << 3);
 }
 
 int main(int argc, char* argv[]) {
-  assert(xbyte(0xAABBCCDD, 1) == 0xFFFFFFCC);
+  assert(xbyte(0x00112233, 0) == 0x33);
+  assert(xbyte(0x00112233, 1) == 0x22);
   assert(xbyte(0x00112233, 2) == 0x11);
+  assert(xbyte(0x00112233, 3) == 0x00);
+
+  assert(xbyte(0xAABBCCDD, 0) == 0xFFFFFFDD);
+  assert(xbyte(0xAABBCCDD, 1) == 0xFFFFFFCC);
+  assert(xbyte(0xAABBCCDD, 2) == 0xFFFFFFBB);
+  assert(xbyte(0xAABBCCDD, 3) == 0xFFFFFFAA);
+
   return 0;
 }
-
 
